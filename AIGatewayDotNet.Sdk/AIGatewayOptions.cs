055bbeffe77cea952a96ebe610e465471866eaf5
Remo <me@imguan.com>
2024-04-15 13:20:54 +0800

    Add validation method to AIGatewayOptions
    Introduced a new method, Validate(), in the AIGatewayOptions class. The method checks for null or white space in several properties in the AIGatewayOptions instance, as well as the validity of the provider. It's now being called in the AIGatewayService constructor for further verifications.

--- AIGatewayDotNet.Sdk/AIGatewayOptions.cs
+++ AIGatewayDotNet.Sdk/AIGatewayOptions.cs
@@ -12,6 +12,54 @@ public record AIGatewayOptions
     public string? AzureResourceName { get; set; }
     public string? AzureApiVersion { get; set; }
     public string? OpenAiOrganization { get; set; }
+
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Provider))
+        {
+            throw new ArgumentNullException(nameof(Provider));
+        }
+
+        if (string.IsNullOrWhiteSpace(ApiKey))
+        {
+            throw new ArgumentNullException(nameof(ApiKey));
+        }
+
+        if (string.IsNullOrWhiteSpace(CloudFlareAccountTag))
+        {
+            throw new ArgumentNullException(nameof(CloudFlareAccountTag));
+        }
+
+        if (string.IsNullOrWhiteSpace(CloudFlareGateway))
+        {
+            throw new ArgumentNullException(nameof(CloudFlareGateway));
+        }
+
+        if (string.IsNullOrWhiteSpace(CloudFlareGatewayVersion))
+        {
+            throw new ArgumentNullException(nameof(CloudFlareGatewayVersion));
+        }
+
+        if (Provider.Equals(StaticValues.Providers.Azure, StringComparison.OrdinalIgnoreCase))
+        {
+            if (string.IsNullOrWhiteSpace(AzureResourceName))
+            {
+                throw new ArgumentNullException(nameof(AzureResourceName));
+            }
+
+            if (string.IsNullOrWhiteSpace(AzureApiVersion))
+            {
+                throw new ArgumentNullException(nameof(AzureApiVersion));
+            }
+        }
+        else if (Provider.Equals(StaticValues.Providers.OpenAi, StringComparison.OrdinalIgnoreCase))
+        {
+        }
+        else
+        {
+            throw new ArgumentException($"Provider {Provider} is not supported");
+        }
+    }
 }
 
 public enum Provider
